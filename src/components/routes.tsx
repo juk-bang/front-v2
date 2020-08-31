2385e159507f7bf8fcf53d6e6e7250f4017dfb52
yejikim9532 <yejikim9532@gmail.com>
2020-09-01 03:52:19 +0900

    라우팅에 회원가입 url 추가

--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { roomUrl, authUrl } from "./urls";
 import roomListContainer from "../pages/room/roomList/roomListContainer";
 import SignIn from "../pages/auth/SignIn";
+import SignUp from "../pages/auth/SignUp";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path={roomUrl.home} exact component={roomListContainer} />
       <Route path={authUrl.signIn} exact component={SignIn} />
+      <Route path={authUrl.signUp} exact component={SignUp} />
     </Switch>
   );
 };
