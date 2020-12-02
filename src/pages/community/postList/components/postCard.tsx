4adf75615a5986cf7549e41b6551d58bf45fe30c
yejikim9532 <yejikim9532@gmail.com>
2020-12-02 17:42:20 +0900

    UI수정 + 접근제한

--- src/pages/community/postList/components/postCard.tsx
+++ src/pages/community/postList/components/postCard.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom";
+import { get_id } from "../../../../API/auth";
 import {communityUrl} from "../../../../components/urls";
 
 interface IProps{
@@ -11,24 +12,19 @@ interface IProps{
 const PostCard: React.FC<IProps> = ({id, post, role}) => {
     const currentUnivid = localStorage.getItem("univid");
 
-    return <Link to={currentUnivid ? role=="all" ? communityUrl.getCommunityDetailAll(parseInt(currentUnivid), post.postId) : communityUrl.getCommunityDetailStudent(parseInt(currentUnivid), post.postId) : ""} 
-                className="w-full h-20 bg-gray-200 flex items-center justify-start pl-10 border-solid border-4 border-gray-600 mt-px">
-        <div className="ml-8">
-            {post.views}
-        </div>
-        <div className="w-2/3 h-full flex items-center justify-center">
-            {post.title}
-        </div>
-        <div className="w-32 h-full flex items-center justify-center">
-            {post.comments}
-        </div>
-        <div>
-            {post.writer}
-        </div>
-        <div className="pl-10">
-            {post.updatedDate.slice(0, 10)}
-        </div>
-    </Link>
+    return <Link to={currentUnivid ? role=="all" ? communityUrl.getCommunityDetailAll(parseInt(currentUnivid), post.postId) : communityUrl.getCommunityDetailStudent(parseInt(currentUnivid), post.postId) : ""}
+        className="grid grid-cols-11">
+            <div className="col-span-5 pl-5 py-3 border-b-2 border-gray-300 text-start text-sm leading-4 tracking-wider"> {post.title}</div>
+            <div className="col-span-1 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider">  {post.comments}</div>
+            <div className="col-span-1 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider"> {post.views}</div>
+            <div className="col-span-2 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider">  {post.writer===undefined?get_id():post.writer}</div>
+            <div className="col-span-2 py-3 border-b-2 border-gray-300 text-center text-sm leading-4 tracking-wider">
+                <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                    <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                    <span className="relative text-xs">{post.updatedDate.slice(0, 10)}</span>
+                </span>
+            </div>
+        </Link>
 }
 
 export default PostCard;
--- src/pages/community/postList/postListPresenter.tsx
+++ src/pages/community/postList/postListPresenter.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../../../components/NavBar";
 import "../../../sass/tailwind.output.css";
 import PostCard from "./components/postCard";
 import {communityUrl} from "../../../components/urls"
+import { get_role, position } from "../../../API/auth";
 
 interface IProps{
     postList:any;
@@ -21,7 +22,23 @@ const PostListPresenter:React.FC<IProps> = ({postList, role, setRole}) => {
         else if (role=="all")
             newPostUrl = communityUrl.getNewCommunityPostAll(parseInt(currentUnivid));
 
+    useEffect(()=>{
+        const st = document.getElementById('student');
+        const al = document.getElementById('all');
+        if(role === "student"){
+            st?.setAttribute('class', "mr-2 cursor-pointer btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-blue-600 hover:bg-blue-900 text-white font-normal py-2 px-4 mr-1 rounded")
+            al?.setAttribute('class',"cursor-pointer btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border bg-black-100 border-blue-700 hover:bg-blue-700 text-blue-700 hover:text-white font-normal py-2 px-4 rounded")
+        }else if(role === "all"){
+           al?.setAttribute('class', "cursor-pointer btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-blue-600 hover:bg-blue-900 text-white font-normal py-2 px-4 mr-1 rounded")
+           st?.setAttribute('class', "mr-2 cursor-pointer btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border bg-black-100 border-blue-700 hover:bg-blue-700 text-blue-700 hover:text-white font-normal py-2 px-4 rounded")
+        }
+    },[role])
+
     const handleChangeStudentRole = (e:any) => {
+        if(get_role() !== position.STUDENT){
+            alert('접근 불가한 페이지 입니다.');
+            return;
+        }
         setRole("student");
         localStorage.setItem("role", "student");
     }
@@ -35,53 +52,33 @@ const PostListPresenter:React.FC<IProps> = ({postList, role, setRole}) => {
     return <>
         <NavBar></NavBar>
         <div className="pt-20 flex items-center justify-center flex-col">
-            <div className="w-2/3 h-16 mt-8 flex items-center justify-between">
+            <div className="mb-2 w-2/3 h-16 mt-8 flex items-center justify-between">
                 <div className="flex">
-                    {role=="student" ?
-                        <div className="w-24 h-12 bg-blue-400 flex items-center justify-center mr-8" onClick={handleChangeStudentRole}>
-                            학생 전용
-                        </div>
-                        :
-                        <div className="w-24 h-12 bg-blue-200 flex items-center justify-center mr-8" onClick={handleChangeStudentRole}>
-                            학생 전용
-                        </div>
-                    }
-                    {role=="all" ?
-                        <div className="w-24 h-12 bg-blue-400 flex items-center justify-center mr-8" onClick={handleChangeAllRole}>
-                            모든 회원
-                        </div>
-                        :
-                        <div className="w-24 h-12 bg-blue-200 flex items-center justify-center mr-8" onClick={handleChangeAllRole}>
-                            모든 회원
-                        </div>
-                    }
+                    <div id = "student" className="mr-2 cursor-pointer btn-outline-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline border bg-black-100 border-blue-700 hover:bg-blue-700 text-blue-700 hover:text-white font-normal py-2 px-4 rounded" onClick={handleChangeStudentRole}>
+                        학생 전용
+                    </div>
+                    <div id = "all" className="cursor-pointer btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-blue-600 hover:bg-blue-900 text-white font-normal py-2 px-4 mr-1 rounded" onClick={handleChangeAllRole}>
+                        모든 회원
+                    </div>
+                    
                 </div>
-                <Link to={newPostUrl} className="w-24 h-12 bg-blue-200 flex items-center justify-center">
+                <Link to={newPostUrl} className="cursor-pointer btn-primary transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-blue-600 hover:bg-blue-900 text-white font-normal py-2 px-4 mr-1 rounded">
                     글쓰기
                 </Link>
             </div>
-            <div className="w-2/3 h-12 bg-blue-200 flex items-center justify-start">
-                <div className="ml-16 mr-48">
-                    조회수
-                </div>
-                <div className="ml-48 mr-48">
-                    제목
-                </div>
-                <div className="ml-56 mr-16">
-                    댓글수
-                </div>
-                <div>
-                    글쓴이
-                </div>
-                <div className="mr-16 ml-16">
-                    날짜
-                </div>
-            </div>
-            <div className="w-2/3 h-screen mt-8 flex items-center justify-start flex-col">
+
+            <div className="w-2/3 shadow-lg overflow-hidden bg-white shadow-dashboard px-8 pt-3 rounded-bl-lg rounded-br-lg">
+                <div className = "grid grid-cols-11 font-bold">            
+                    <div className="col-span-5 py-3 border-b-2 border-gray-400 text-center text-sm leading-4 text-blue-500 tracking-wider">제목</div>
+                    <div className="col-span-1 py-3 border-b-2 border-gray-400 text-center text-sm leading-4 text-blue-500 tracking-wider">조회수</div>
+                    <div className="col-span-1 py-3 border-b-2 border-gray-400 text-center text-sm leading-4 text-blue-500 tracking-wider">댓글수</div>
+                    <div className="col-span-2 py-3 border-b-2 border-gray-400 text-center text-sm leading-4 text-blue-500 tracking-wider">작성자</div>
+                    <div className="col-span-2  py-3 border-b-2 border-gray-400 text-center text-sm leading-4 text-blue-500 tracking-wider">날짜</div>  
+                </div>                        
                 {
                     postList ?
-                    postList.map((post:any) => <PostCard id={post.postId} post={post} role={role}/> ) : ""
-                }
+                    postList.map((post:any) => <PostCard id={post.postId} post={post} role={post.role}/>  ) : ""
+                }  
             </div>
         </div>
     </>
